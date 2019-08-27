2f16c297f0c1f021309e2b52e03f5e2a19709e35
zaptrem <zaptrem@me.com>
2019-08-26 23:14:30 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export const selectRecording = (recording) => {
-	consol.log("You clicked on recording: ", recording.id);
-	return {
-		type: "RECORDING_SELECTED",
-		//data
-		payload: recording
-	}
-};
