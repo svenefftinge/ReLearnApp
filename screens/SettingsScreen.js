b8eaafd3c5465dd04335f5139b6564b0db697f22
zaptrem <zaptrem@me.com>
2019-09-05 23:54:34 +0000

    Fixed settings by removing it

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -3,12 +3,14 @@ import { ExpoConfigView } from '@expo/samples';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    //title: 'app.json',
+    title: 'Settings'
   };
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    //return <ExpoConfigView />;
+    return null;
   }
 }
