028758014513bb3dcfb9991d6e1b3257b39d8b52
zaptrem <zaptrem@me.com>
2019-09-08 20:59:02 +0000

    Redux Thunk now hooked up to doc picker.

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,50 @@
+import * as DocumentPicker from 'expo-document-picker';
 // Action Creator
 export const addRecording = (recording) => {
-    console.log("You added a recording: ", recording.id);
+    console.log("You added a recording: ", recording);
     // The Action being returned
     return {
         type: "RECORDING_ADDED",
         payload: recording
     }
 };
+export function addRecordingFile() {
+    // We can invert control here by returning a function - the "thunk".
+    // When this function is passed to `dispatch`, the thunk middleware will intercept it,
+    // and call it with `dispatch` and `getState` as arguments.
+    // This gives the thunk function the ability to run some logic, and still interact with the store.
+    async function pickRecording() {
+        let result = await DocumentPicker.getDocumentAsync({ type: '*/*' });
+
+        if (result.uri) {
+            //upload function(result.uri);
+            //return result;
+            console.log(result);
+            return {
+                eventID: "0",
+                id: "12345abaccaa",
+                name: "Whitee Hall",
+                date: new Date('1995-12-17T03:24:01'),
+                status: "loading",
+                recordings: 5,
+                location: "White Hall",
+                localTranscription: "The lazy cat jumped over the dog.",
+                serverTranscription: ""
+            };
+        } else {
+            throw "nothing selected";
+        }
+    }
+    return function(dispatch) {
+        return pickRecording().then(
+            (result) => {
+                        dispatch(addRecording(result));
+
+                        //Result is in the system now, populate it.
+                        console.log("it works");
+                        },
+            (error) => console.log(error),
+        );
+    };
+}
+
--- components/upload-recording-button.js
+++ components/upload-recording-button.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import * as DocumentPicker from 'expo-document-picker';
 import { bindActionCreators } from 'redux';
-import { addRecording } from '../actions/index.js';
+import { addRecordingFile } from '../actions/index.js';
 import { Button } from 'react-native';
 import { connect } from 'react-redux';
 
@@ -9,21 +8,7 @@ class UploadRecordingButton extends Component {
     render() {
         return (
             <Button
-                //onPress={()=>uploadRecording()}
-                //onPress={()=>addToListActionCreator()}
-                onPress={() => this.props.addRecording(
-                    {
-                        eventID: "0",
-                        id: "2940189248",
-                        name: "Whitee Hall",
-                        date: new Date('1995-12-17T03:24:02'),
-                        status: "recording",
-                        recordings: 6,
-                        location: "White Hall",
-                        localTranscription: "The lazyy cat jumped over the dog.",
-                        serverTranscription: ""
-                    }
-                )}
+                onPress={()=>this.props.addRecordingFile()}
                 title="Add"
                 color="#007AFF"
             />
@@ -31,33 +16,8 @@ class UploadRecordingButton extends Component {
     }
 }
 
-function addToListActionCreator() {
-    return [
-        {
-            eventID: "0",
-            id: "12345abacca",
-            name: "White Hall",
-            date: new Date('1995-12-17T03:24:01'),
-            status: "recording",
-            recordings: 5,
-            location: "White Hall",
-            localTranscription: "The lazy cat jumped over the dog.",
-            serverTranscription: ""
-        }
-
-    ]
-
-}
-async function uploadRecording() {
-    let result = await DocumentPicker.getDocumentAsync({ type: '*/*' });
-
-    if (!result.cancelled) {
-        //upload function(result.uri);
-    }
-}
-
 function matchDispatchToProps(dispatch) {
-    return bindActionCreators({ addRecording: addRecording }, dispatch)
+    return bindActionCreators({ addRecordingFile: addRecordingFile }, dispatch)
 }
 
 export default connect(null, matchDispatchToProps)(UploadRecordingButton);
